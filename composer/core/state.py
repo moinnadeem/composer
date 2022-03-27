84529d9ee85188bcf8ab3ec52d0e40811a75070f
Moin Nadeem <moin@mosaicml.com>
2022-03-27 20:55:09 +0000

    updating backwards compatbility on checkpointing

--- composer/core/state.py
+++ composer/core/state.py
@@ -320,13 +320,14 @@ class State(Serializable):
             strict (bool): Whether the keys (i.e., model parameter names) in the model state dict should
                 perfectly match the keys in the model instance.
         """
+        state_dict = _ensure_backwards_compatible_checkpointing(state_dict)
         if state_dict["is_model_ddp"] and not self.is_model_ddp:
             torch.nn.modules.utils.consume_prefix_in_state_dict_if_present(state_dict['model'], "module.")
 
         # if necessary, delete some keys from the state_dict
-        if ignore_model_keys is not None and len(ignore_model_keys) > 0:
-            for key in ignore_model_keys:
-                del state_dict['model'][key]
+        # if ignore_model_keys is not None and len(ignore_model_keys) > 0:
+            # for key in ignore_model_keys:
+                # del state_dict['model'][key]
 
         # load checkpoint
         missing_keys, unexpected_keys = self.model.load_state_dict(state_dict['model'], strict=strict)
